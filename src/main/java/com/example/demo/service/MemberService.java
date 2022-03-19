c9917c55a8152a7b7d87550d49db7bc59a79e609
GregLee <greg.lee@nsuslab.com>
2022-03-20 00:49:15 +0900

    no message

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -3,13 +3,19 @@ package com.example.demo.service;
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
@@ -24,7 +30,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).
                 ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원");
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
